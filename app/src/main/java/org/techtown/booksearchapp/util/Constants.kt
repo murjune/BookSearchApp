efea92a05cefd55eea09d9663041ec47a284963a
murjune <sis9221302@gmail.com>
2022-06-24 20:03:22 +0900

    [FEAT] BookSearchAPI 서버통신 로직 구현완료

--- app/src/main/java/org/techtown/booksearchapp/util/Constants.kt
+++ app/src/main/java/org/techtown/booksearchapp/util/Constants.kt
@@ -3,6 +3,6 @@ package org.techtown.booksearchapp.util
 import org.techtown.booksearchapp.BuildConfig
 
 object Constants {
-    const val BASE_URL = "https://dapi.kakao.com"
+    const val BASE_URL: String = "https://dapi.kakao.com/"
     const val API_KEY = BuildConfig.bookAPIKey
 }
