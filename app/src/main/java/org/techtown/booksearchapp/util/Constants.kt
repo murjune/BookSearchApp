8d834ee7893f7340012624917b5296ec401e4a7a
murjune <sis9221302@gmail.com>
2022-06-24 19:40:26 +0900

    [FEAT] Constants.kt 생성 및 API_KEY 숨기기 구현

---
+++ app/src/main/java/org/techtown/booksearchapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package org.techtown.booksearchapp.util
+
+import org.techtown.booksearchapp.BuildConfig
+
+object Constants {
+    const val BASE_URL = "https://dapi.kakao.com"
+    const val API_KEY = BuildConfig.bookAPIKey
+}
