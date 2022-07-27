db7f015935ff017715f1480b3f943648fe0c187a
murjune <sis9221302@gmail.com>
2022-07-27 17:57:03 +0900

    [feat] BookFragment에 args에 담긴 Book 객체를 이용해서 WebView Setting

--- app/src/main/java/org/techtown/booksearchapp/ui/view/BookFragment.kt
+++ app/src/main/java/org/techtown/booksearchapp/ui/view/BookFragment.kt
@@ -1,16 +1,21 @@
 package org.techtown.booksearchapp.ui.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import org.techtown.booksearchapp.databinding.FragmentBookBinding
 
 class BookFragment : Fragment() {
 
-    var _binding: FragmentBookBinding? = null
-    val binding get() = _binding ?: error("바인딩에 null이 들어감")
+    private var _binding: FragmentBookBinding? = null
+    private val binding get() = _binding ?: error("바인딩에 null이 들어감")
+    private val args by navArgs<BookFragmentArgs>()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,6 +23,33 @@ class BookFragment : Fragment() {
     ): View? {
         _binding = FragmentBookBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initWebPage()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebPage() {
+        val book = args.book
+
+        binding.wbBook.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl(book.url)
+        }
+    }
+
+    override fun onPause() {
+        binding.wbBook.onPause()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.wbBook.onResume()
     }
 
     override fun onDestroyView() {
