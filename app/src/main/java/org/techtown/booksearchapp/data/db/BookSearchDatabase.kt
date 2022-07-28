cea3fcdb0870babd2a5ab803ea329726fa3a9a02
murjune <sis9221302@gmail.com>
2022-07-29 05:59:19 +0900

    [feat] BookSearch DB  추상클래스 구현(Retrofit 구현할 때랑 비슷하다)

---
+++ app/src/main/java/org/techtown/booksearchapp/data/db/BookSearchDatabase.kt
@@ -0,0 +1,34 @@
+package org.techtown.booksearchapp.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import org.techtown.booksearchapp.data.model.Book
+
+@Database(
+    entities = [Book::class],
+    version = 1,
+    exportSchema = false // 얘는 뭘까
+)
+abstract class BookSearchDatabase : RoomDatabase() {
+
+    abstract fun bookSearchDao(): BookSearchDao
+
+    companion object { // singleton 설정
+        @Volatile
+        private var INSTANCE: BookSearchDatabase? = null
+
+        private fun buildDatabase(context: Context): BookSearchDatabase =
+            Room.databaseBuilder(
+                context.applicationContext,
+                BookSearchDatabase::class.java,
+                "favorite-books"
+            ).build()
+
+        fun getInstance(context: Context): BookSearchDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+            }
+    }
+}
