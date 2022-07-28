bf54a08b9d2ac723ff88ff42bbd37d0f0c6c0955
murjune <sis9221302@gmail.com>
2022-07-29 06:16:33 +0900

    [feat] Repository에 db 주입

--- app/src/main/java/org/techtown/booksearchapp/data/repository/BookSearchRepository.kt
+++ app/src/main/java/org/techtown/booksearchapp/data/repository/BookSearchRepository.kt
@@ -1,10 +1,22 @@
 package org.techtown.booksearchapp.data.repository
 
+import androidx.lifecycle.LiveData
+import org.techtown.booksearchapp.data.model.Book
 import org.techtown.booksearchapp.data.model.SearchResponse
 import retrofit2.Response
 
 interface BookSearchRepository {
+
+    // remote
     suspend fun searchBooks(
         query: String
     ): Response<SearchResponse>
+
+    // Room
+
+    suspend fun insertBooks(book: Book)
+
+    suspend fun deleteBooks(book: Book)
+
+    suspend fun getFavoriteBooks(): LiveData<List<Book>>
 }
--- app/src/main/java/org/techtown/booksearchapp/data/repository/BookSearchRepositoryImpl.kt
+++ app/src/main/java/org/techtown/booksearchapp/data/repository/BookSearchRepositoryImpl.kt
@@ -1,12 +1,32 @@
 package org.techtown.booksearchapp.data.repository
 
+import androidx.lifecycle.LiveData
 import org.techtown.booksearchapp.data.api.SearchBookClient
+import org.techtown.booksearchapp.data.db.BookSearchDatabase
+import org.techtown.booksearchapp.data.model.Book
 import org.techtown.booksearchapp.data.model.SearchResponse
 import retrofit2.Response
 
-class BookSearchRepositoryImpl : BookSearchRepository {
+class BookSearchRepositoryImpl(
+    private val db: BookSearchDatabase
+) : BookSearchRepository {
+
+    // remote
     override suspend fun searchBooks(query: String): Response<SearchResponse> {
         return SearchBookClient.bookSearchApi.searchBooks(query)
+    }
+
+    // Room
+    override suspend fun insertBooks(book: Book) {
+        db.bookSearchDao().insertBook(book)
+    }
+
+    override suspend fun deleteBooks(book: Book) {
+        db.bookSearchDao().deleteBook(book)
+    }
+
+    override suspend fun getFavoriteBooks(): LiveData<List<Book>> {
+        return db.bookSearchDao().getFavoriteBooks()
     }
     // api -> repository -> model -> view
 }
