51e815bf6e1956edf7031599143a90d7482e4dfd
murjune <sis9221302@gmail.com>
2022-08-11 05:14:46 +0900

    [chore] 함수 네이밍 변경

--- app/src/main/java/org/techtown/booksearchapp/di/RepositoryModule.kt
+++ app/src/main/java/org/techtown/booksearchapp/di/RepositoryModule.kt
@@ -7,12 +7,12 @@ import dagger.hilt.android.components.ViewModelComponent
 import org.techtown.booksearchapp.data.repository.BookSearchRepository
 import org.techtown.booksearchapp.data.repository.BookSearchRepositoryImpl
 
-@Module
-@InstallIn(ViewModelComponent::class)
+@Module // @Module : hilt가 알 수 있도록 hilt-module임을 가리킴
+@InstallIn(ViewModelComponent::class) // @InstallIn : @Module로 지정된 Class는 @Installin을 지정하여 각 모듈이 어떤 Scope(activity, fragment...)에서 사용되는지 알려야함.
 abstract class RepositoryModule {
 
     @Binds
-    abstract fun providesRulesTodayRepository(
+    abstract fun bindBookSearchRepository(
         impl: BookSearchRepositoryImpl
     ): BookSearchRepository
 }
