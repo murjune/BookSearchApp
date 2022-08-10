c802e70437a29e596d8d70f2865a4cbea7a498c8
murjune <sis9221302@gmail.com>
2022-08-10 21:34:45 +0900

    [fix] @Headers 제거

--- app/src/main/java/org/techtown/booksearchapp/data/api/BookSearchApi.kt
+++ app/src/main/java/org/techtown/booksearchapp/data/api/BookSearchApi.kt
@@ -1,14 +1,11 @@
 package org.techtown.booksearchapp.data.api
 
 import org.techtown.booksearchapp.data.model.SearchResponse
-import org.techtown.booksearchapp.util.Constants.API_KEY
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface BookSearchApi {
-    @Headers("Authorization: KakaoAK $API_KEY")
     @GET("v3/search/book")
     suspend fun searchBooks(
         @Query("query") query: String
