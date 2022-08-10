d2df6a9dbcf7bf5cd32e8159b1610ee7d7c3cc01
murjune <sis9221302@gmail.com>
2022-08-10 21:37:19 +0900

    [feat] NetWorkModule 추가

---
+++ app/src/main/java/org/techtown/booksearchapp/di/NetworkModule.kt
@@ -0,0 +1,74 @@
+package org.techtown.booksearchapp.di
+
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.techtown.booksearchapp.BuildConfig
+import org.techtown.booksearchapp.data.api.BookSearchApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+    private const val BASE_URL: String = BuildConfig.BASE_URL
+    private const val API_KEY = BuildConfig.bookAPIKey
+
+    @Singleton
+    @Provides
+    fun provideBookSearchApi(retrofit: Retrofit): BookSearchApi =
+        retrofit.create(BookSearchApi::class.java)
+
+    @Provides
+    @Singleton
+    fun providesBookSearchRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(
+                GsonConverterFactory.create(
+                    GsonBuilder().serializeNulls().create()
+                )
+            )
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(interceptor: Interceptor): OkHttpClient = OkHttpClient.Builder()
+        .connectTimeout(15, TimeUnit.SECONDS)
+        .writeTimeout(20, TimeUnit.SECONDS)
+        .readTimeout(15, TimeUnit.SECONDS)
+        .addInterceptor(interceptor) // Header 인턴셉터 추가
+        .addInterceptor(
+            // 로깅 인턴셉터 추가
+            HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+        )
+        .build()
+
+    @Provides
+    @Singleton
+    fun providesHousInterceptor(): Interceptor =
+        Interceptor { chain ->
+            with(chain) {
+                proceed(
+                    request()
+                        .newBuilder()
+                        .addHeader(
+                            "Authorization",
+                            API_KEY
+                        )
+                        .addHeader("Content-Type", "Application/json")
+                        .build()
+                )
+            }
+        }
+}
