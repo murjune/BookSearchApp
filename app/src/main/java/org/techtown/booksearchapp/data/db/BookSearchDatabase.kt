ae8800df52fc68625e09e076f611c4cfc39ecc64
murjune <sis9221302@gmail.com>
2022-08-11 05:11:10 +0900

    [feat] DataBaseModule 추가

--- app/src/main/java/org/techtown/booksearchapp/data/db/BookSearchDatabase.kt
+++ app/src/main/java/org/techtown/booksearchapp/data/db/BookSearchDatabase.kt
@@ -1,8 +1,6 @@
 package org.techtown.booksearchapp.data.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import org.techtown.booksearchapp.data.model.Book
@@ -16,21 +14,4 @@ import org.techtown.booksearchapp.data.model.Book
 abstract class BookSearchDatabase : RoomDatabase() {
 
     abstract fun bookSearchDao(): BookSearchDao
-
-    companion object { // singleton 설정
-        @Volatile
-        private var INSTANCE: BookSearchDatabase? = null
-
-        private fun buildDatabase(context: Context): BookSearchDatabase =
-            Room.databaseBuilder(
-                context.applicationContext,
-                BookSearchDatabase::class.java,
-                "favorite-books"
-            ).build()
-
-        fun getInstance(context: Context): BookSearchDatabase =
-            INSTANCE ?: synchronized(this) {
-                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
-            }
-    }
 }
---
+++ app/src/main/java/org/techtown/booksearchapp/di/DataBaseModule.kt
@@ -0,0 +1,33 @@
+package org.techtown.booksearchapp.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import org.techtown.booksearchapp.data.db.BookSearchDao
+import org.techtown.booksearchapp.data.db.BookSearchDatabase
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DataBaseModule {
+
+    @Provides
+    fun provideBookDao(bookSearchDatabase: BookSearchDatabase): BookSearchDao {
+        return bookSearchDatabase.bookSearchDao()
+    }
+
+    @Provides
+    @Singleton
+    // Hilt에서 항상 동일한 데이터베이스 인스턴스를 제공하도록 Singleton으로
+    fun provideDatabas(@ApplicationContext appContext: Context): BookSearchDatabase {
+        return Room.databaseBuilder(
+            appContext.applicationContext,
+            BookSearchDatabase::class.java,
+            "favorite-books"
+        ).build()
+    }
+}
