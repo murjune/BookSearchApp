c865762c92727aacbc3d61d0ee9392d8c280b772
murjune <sis9221302@gmail.com>
2022-09-11 22:56:17 +0900

    [feat] dataStore 모듈 구현

--- app/src/main/java/org/techtown/booksearchapp/di/DataStoreModule.kt
+++ app/src/main/java/org/techtown/booksearchapp/di/DataStoreModule.kt
@@ -1,4 +1,22 @@
 package org.techtown.booksearchapp.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import org.techtown.booksearchapp.util.dataStore
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
 object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
+        context.dataStore
 }
