8aaaf4d7b4717890c747ca1e139e2da3ad084d6d
murjune <sis9221302@gmail.com>
2022-09-11 22:57:11 +0900

    [feat] BookSearchRepo에 preference dataStore에 data 저장/수정 로직 구현

--- app/src/main/java/org/techtown/booksearchapp/data/repository/BookSearchRepositoryImpl.kt
+++ app/src/main/java/org/techtown/booksearchapp/data/repository/BookSearchRepositoryImpl.kt
@@ -1,23 +1,40 @@
 package org.techtown.booksearchapp.data.repository
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import org.techtown.booksearchapp.data.datasource.RemoteDataSource
 import org.techtown.booksearchapp.data.db.BookSearchDao
 import org.techtown.booksearchapp.data.model.Book
+import org.techtown.booksearchapp.util.Sort
 import org.techtown.booksearchapp.util.UiState
 import timber.log.Timber
+import java.io.IOException
 import javax.inject.Inject
 
 class BookSearchRepositoryImpl @Inject constructor(
     private val bookSearchDao: BookSearchDao,
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val dataStore: DataStore<Preferences>
 ) : BookSearchRepository {
 
     // remote
-    override fun searchBooks(query: String): Flow<UiState<List<Book>>> {
+    override fun searchBooks(
+        query: String,
+        sort: String,
+        page: Int,
+        size: Int
+    ): Flow<UiState<List<Book>>> {
         return flow {
-            runCatching { remoteDataSource.searchBooks(query) }.fold(
+            runCatching { remoteDataSource.searchBooks(query, sort, page, size) }.fold(
                 {
                     val books = it.books
 
@@ -42,5 +59,29 @@ class BookSearchRepositoryImpl @Inject constructor(
 
     override fun getFavoriteBooks(): Flow<List<Book>> {
         return bookSearchDao.getFavoriteBooks()
+    }
+
+    // DataStore
+    override suspend fun saveSortMode(mode: String) {
+        dataStore.edit { prefs ->
+            Timber.d("mode : $mode")
+            prefs[SORT_MODE] = mode
+        }
+    }
+
+    override fun getSortMode(): Flow<String> {
+        return dataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    Timber.e(exception.message)
+                    emit(emptyPreferences())
+                }
+            }.map { prefs ->
+                prefs[SORT_MODE] ?: Sort.ACCURACY.value
+            }.flowOn(Dispatchers.IO)
+    }
+
+    private companion object {
+        val SORT_MODE = stringPreferencesKey("sort_mode")
     }
 }
